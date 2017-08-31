608d016155b690ec9275d975ba9ed066d56670e7
zadesrujan <31057499+zadesrujan@users.noreply.github.com>
2017-08-31 12:51:34 +0530

    Add files via upload

---
+++ Myname.java
@@ -0,0 +1,16 @@
+
+public class Myname {
+
+	public static void main(String[] args) {
+		//public : it is a access specifier that means it will be accessed by publically.
+		//static : it is access modifier that means when the java program is load then it will create the space in memory automatically.
+		//void : it is a return type that is it does not return any value.
+		//main : it is a method or a function name.
+		//string args[] : its a command line argument it is a collection of variables in the string format.
+	System.out.println("hello world");
+		//system : system is a class in java language.
+		//out : out is the static member of system class.
+		//println : which is used to print the message at output.
+	}
+
+}
